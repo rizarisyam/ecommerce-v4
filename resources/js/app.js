4655d66e1f293e461332ff7b6e70bb3c70c99979
rizarisyam <risyam23riza@gmail.com>
2022-01-31 01:59:46 +0700

    deploy

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 
-
 import store from "./Store";
 import PrimeVue from "primevue/config";
 import ConfirmationService from "primevue/confirmationservice";
